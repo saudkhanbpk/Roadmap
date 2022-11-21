7338ba8c2b5100b87f79212e80dcc71e2edf2124
@imrookhan <khannihar921@gmail.com>
2022-11-21 11:28:09 +0500

    change the repo

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,28 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import cors from 'cors';
-
+import * as Store from 'connect-redis';
+import * as session from 'express-session';
+import { redis } from './Redis';
 async function bootstrap() {
+  const RedisStore = Store(session);
   const app = await NestFactory.create(AppModule, { cors: true });
 
-  app.use(cookieParser());
+
+
+
+
+
+  app.use(cookieParser(), session({
+    store: new RedisStore({
+      client: redis as any,
+    }),
+    name: 'qid',
+    secret: 'asldkfjalksjdf',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { httpOnly: true, secure: false,},
+  }));
 
   // app.enableCors({
   //   origin: 'http://localhost:8000',
